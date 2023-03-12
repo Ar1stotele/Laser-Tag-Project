0854d25225c9fd1b38d34c0c81990d2bd12c6bdf
zanek116 <124922869+zanek116@users.noreply.github.com>
2023-03-12 17:19:30 -0500

    Update tktest.py

--- src/tktest.py
+++ src/tktest.py
@@ -1,57 +1,34 @@
-
 import database
 import tkinter as tk
 from tkinter import *
 from PIL import ImageTk, Image
 import constants as config
+import customtkinter
+import keyboard
 
 
 ##TkInter's instance 
 root = tk.Tk()
 
-def new_window(self):
-  #open a new window for play action screen and begin display pregame countdown
-  self.play_action = tk.Tk()
-  self.play_action.title("Play Action")
-  self.play_action.geometry("900x800")
-
-
-
-	#Frames for holding teams
-  team1_container=Frame(self.play_action, relief="sunken", borderwidth=2)
-  team1_container.pack(side="left", fill="x")
-
-  team2_container=Frame(self.play_action, relief="sunken", borderwidth=2)
-  team2_container.pack(side="right", fill="x")
-
-
-
-	#Team titles
-  team1Label = Label(team1_container, text="Red Team", bg = "Red")
-  team2Label = Label(team2_container, text="Green Team", bg = "Green")
-
-  team1Label.pack(side = "top")
-  team2Label.pack(side = "top")
-
-  for x in range(19):
-			playerEntry	(team1_container, x+1)
-			playerEntry(team2_container, x+1)
-
-    
 
 #Creates a single player entry box
 class playerEntry:
 
 	def __init__(self, container, playernum):
-	
+				
 		self.PlayerID = None
 		self.Codename = None
 		
 		self.frame = Frame(container, relief="sunken", borderwidth=2)
 		self.frame.pack(side = "top")
 		
-		self.P1 = Label(self.frame, text=playernum)
-		self.P1.pack(side = "left")
+		if (playernum>9):
+			self.P1 = Label(self.frame, text=str(playernum) + " " )
+			self.P1.pack(side = "left")
+		else:
+			self.P1 = Label(self.frame, text=playernum) # >> fix this
+			self.P1.pack(side = "left")
+			
 		
 		self.L1 = Label(self.frame, text="Player ID:")
 		self.L1.pack(side = "left")
@@ -59,7 +36,8 @@ class playerEntry:
 		self.E1 = Entry(self.frame)
 		self.E1.pack(side = "left")
 		
-		self.B1 = Button(self.frame, text = ">", command = self.getPlayer)
+		self.B1 = customtkinter.CTkButton(self.frame, text = ">", command = self.getPlayer, width = 10, height = 10, fg_color = "red")
+		self.B1.place(relx=10, rely=10, anchor=tk.CENTER)
 		self.B1.pack(side = "left")
 		
 		self.B2 = Button(self.frame, text = ">", command = self.newCodename, state = "disabled")
@@ -70,11 +48,6 @@ class playerEntry:
 		
 		self.L2 = Label(self.frame, text="Codename:")
 		self.L2.pack(side = "right")
-
-
-
-
-		
 		
 		
 	
@@ -83,10 +56,10 @@ class playerEntry:
 		self.PlayerID = self.E1.get()
 		
 		#Disable the playerID entry box
-		self.E1.config(state = "disabled")
-		self.B1.config(state = "disabled")
+		self.E1.configure(state = "disabled")
+		self.B1.configure(state = "disabled")
 		
-		self.B2.config(state = "active")
+		self.B2.configure(state = "active")
 		
 		#If player already exists in database, get the codename
 		#If the player does not exist, player ID is added to the database 
@@ -136,6 +109,27 @@ class GUI:
 		label_splash_screen = tk.Label(image=self.splash_screen_image)
 		label_splash_screen.grid(column=0, row=0)
 
+	def new_window(self):
+  #open a new window for play action screen and begin display pregame countdown
+		self.play_action = tk.Tk()
+		self.play_action.title("Play Action")
+		self.play_action.geometry("900x800")
+	#Frames for holding teams
+		team1_container=Frame(self.play_action, relief="sunken", borderwidth=2)
+		team1_container.pack(side="left", fill="x")
+		team2_container=Frame(self.play_action, relief="sunken", borderwidth=2)
+		team2_container.pack(side="right", fill="x")
+	#Team titles
+		team1Label = Label(team1_container, text="Red Team", bg = "Red")
+		team2Label = Label(team2_container, text="Green Team", bg = "Green")
+		team1Label.pack(side = "top")
+		team2Label.pack(side = "top")
+		for x in range(19):
+			playerEntry (team1_container, x+1)
+			playerEntry(team2_container, x+1)
+
+
+
 	def main_window(self):
 		# destroying splash screen
 		root.destroy()
@@ -145,27 +139,40 @@ class GUI:
 		main_root.title(config.SCREEN_NAME_PLAYER)
 		main_root.geometry("900x800")
 		
-		#Frames for holding teams
+  
+		timer_container=Frame(main_root, relief="sunken", borderwidth=2)
+		timer_container.pack(side="top", fill="x")
+		
+  
+  		#Frames for holding teams
 		team1_container=Frame(main_root, relief="sunken", borderwidth=2)
 		team1_container.pack(side="left", fill="x")
 		
 		team2_container=Frame(main_root, relief="sunken", borderwidth=2)
 		team2_container.pack(side="right", fill="x")
-
-		play_button = Button(main_root, relief = "sunken", borderwidth=2, text="Lock in Teams", command = lambda: new_window(self))
-		play_button.pack(side="top", pady= 40)
-
 		
 		#Team titles
 		team1Label = Label(team1_container, text="Red Team", bg = "Red")
 		team2Label = Label(team2_container, text="Green Team", bg = "Green")
+		timerLabel = Label(timer_container, text="Timer", bg = "White")
+
 		
 		team1Label.pack(side = "top")
 		team2Label.pack(side = "top")
-		
-		
-		#create 19 players for each team
+		timerLabel.pack(side = "top")
+  
+		def countdown(count):
+			timerLabel['text'] = f'Time left: {count} seconds'	
+			count-=1
+			if count>=0:
+				timerLabel.after(1000,countdown,count)
+		time_left = 360
+		countdown(time_left)
 		
 		for x in range(19):
 			playerEntry(team1_container, x+1)
 			playerEntry(team2_container, x+1)
+   
+		
+
+
